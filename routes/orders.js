8b49607ed45943a9c38c4f3c4ce126eef9a6278d
danielbutts <daniel.butts@gmail.com>
2017-04-16 22:26:46 -0700

    stubbed out routes and templates for donuts and orders.

--- routes/orders.js
+++ routes/orders.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const donuts = {};
-  res.render('donuts', donuts);
+  const orders = {};
+  res.render('orders', orders);
 });
 
 router.delete('/:id', function(req, res, next) {
-  // de-activate donut
-  res.redirect('/donuts');
+  // de-activate an order
+  res.redirect('/orders');
 });
 
 router.post('/', function(req, res, next) {
-  // create a new donut
-  res.redirect('/donuts');
+  // create a new order
+  res.redirect('/orders');
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('/donuts/new');
+  res.render('/orders/new');
 });
 
 router.get('/edit/:id', function(req, res, next) {
   const id = req.params.id;
   const donut = {};
-  res.render(`/donuts/${id}/edit`, donut);
+  res.render(`/orders/${id}/edit`, donut);
 });
 
 router.put('/:id', function(req, res, next) {
-  res.redirect('/donuts');
+  res.redirect('/orders');
 });
 
 module.exports = router;
